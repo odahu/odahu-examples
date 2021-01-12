c2ad03f53cb9644b772e1ce74f930424dedd0a91
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2021-01-12 15:04:58 +0300

    example triton

---
+++ mlflow/sklearn-triton/wine/airflow/dag.py
@@ -0,0 +1,153 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.models import Variable
+from odahuflow.sdk.models import ModelTraining, ModelTrainingSpec, ModelIdentity, ResourceRequirements, ResourceList, \
+    ModelPackaging, ModelPackagingSpec, Target, ModelDeployment, ModelDeploymentSpec, Connection, ConnectionSpec, \
+    DataBindingDir
+
+from odahuflow.airflow_plugin.deployment import DeploymentOperator, DeploymentSensor
+from odahuflow.airflow_plugin.model import ModelPredictRequestOperator, ModelInfoRequestOperator
+from odahuflow.airflow_plugin.packaging import PackagingOperator, PackagingSensor
+from odahuflow.airflow_plugin.training import TrainingOperator, TrainingSensor
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 9, 3),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'end_date': datetime(2099, 12, 31)
+}
+
+api_connection_id = "odahuflow_api"
+model_connection_id = "odahuflow_model"
+
+
+training_id = "airflow-wine"
+training = ModelTraining(
+    id=training_id,
+    spec=ModelTrainingSpec(
+        model=ModelIdentity(
+            name="wine",
+            version="1.0"
+        ),
+        toolchain="mlflow",
+        entrypoint="main",
+        work_dir="mlflow/sklearn/wine",
+        hyper_parameters={
+            "alpha": "1.0"
+        },
+        data=[
+            DataBindingDir(
+                conn_name='wine',
+                local_path='mlflow/sklearn/wine/wine-quality.csv'
+            ),
+        ],
+        resources=ResourceRequirements(
+            requests=ResourceList(
+                cpu="2024m",
+                memory="2024Mi"
+            ),
+            limits=ResourceList(
+                cpu="2024m",
+                memory="2024Mi"
+            )
+        ),
+        vcs_name="odahu-flow-examples"
+    ),
+)
+
+packaging_id = "airflow-wine"
+packaging = ModelPackaging(
+    id=packaging_id,
+    spec=ModelPackagingSpec(
+        integration_name="docker-rest"
+    ),
+)
+
+deployment_id = "airflow-wine"
+deployment = ModelDeployment(
+    id=deployment_id,
+    spec=ModelDeploymentSpec(
+        min_replicas=1,
+    ),
+)
+
+model_example_request = {
+    "columns": ["alcohol", "chlorides", "citric acid", "density", "fixed acidity", "free sulfur dioxide", "pH",
+                "residual sugar", "sulphates", "total sulfur dioxide", "volatile acidity"],
+    "data": [[12.8, 0.029, 0.48, 0.98, 6.2, 29, 3.33, 1.2, 0.39, 75, 0.66],
+             [12.8, 0.029, 0.48, 0.98, 6.2, 29, 3.33, 1.2, 0.39, 75, 0.66]]
+}
+
+dag = DAG(
+    'airflow-wine',
+    default_args=default_args,
+    schedule_interval=None
+)
+
+with dag:
+    train = TrainingOperator(
+        task_id="training",
+        api_connection_id=api_connection_id,
+        training=training,
+        default_args=default_args
+    )
+
+    wait_for_train = TrainingSensor(
+        task_id='wait_for_training',
+        training_id=training_id,
+        api_connection_id=api_connection_id,
+        default_args=default_args
+    )
+
+    pack = PackagingOperator(
+        task_id="packaging",
+        api_connection_id=api_connection_id,
+        packaging=packaging,
+        trained_task_id="wait_for_training",
+        default_args=default_args
+    )
+
+    wait_for_pack = PackagingSensor(
+        task_id='wait_for_packaging',
+        packaging_id=packaging_id,
+        api_connection_id=api_connection_id,
+        default_args=default_args
+    )
+
+    dep = DeploymentOperator(
+        task_id="deployment",
+        api_connection_id=api_connection_id,
+        deployment=deployment,
+        packaging_task_id="wait_for_packaging",
+        default_args=default_args
+    )
+
+    wait_for_dep = DeploymentSensor(
+        task_id='wait_for_deployment',
+        deployment_id=deployment_id,
+        api_connection_id=api_connection_id,
+        default_args=default_args
+    )
+
+    model_predict_request = ModelPredictRequestOperator(
+        task_id="model_predict_request",
+        model_deployment_name=deployment_id,
+        api_connection_id=api_connection_id,
+        model_connection_id=model_connection_id,
+        request_body=model_example_request,
+        default_args=default_args
+    )
+
+    model_info_request = ModelInfoRequestOperator(
+        task_id='model_info_request',
+        model_deployment_name=deployment_id,
+        api_connection_id=api_connection_id,
+        model_connection_id=model_connection_id,
+        default_args=default_args
+    )
+    train >> wait_for_train >> pack >> wait_for_pack >> dep >> wait_for_dep
+    wait_for_dep >> model_info_request
+    wait_for_dep >> model_predict_request
---
+++ mlflow/sklearn-triton/wine/train.py
@@ -0,0 +1,74 @@
+# The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
+# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
+# Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
+
+import argparse
+import os
+import warnings
+
+import mlflow.sklearn
+import numpy as np
+import pandas as pd
+import os
+from sklearn.linear_model import ElasticNet
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from joblib import dump
+
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+output_dir = os.environ.get("ODAHUFLOW_OUTPUT_DIR")
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--alpha')
+    parser.add_argument('--l1-ratio')
+
+    try:
+        args = parser.parse_args()
+    except:
+        args = argparse.Namespace(alpha=0.1, l1_ratio=0.5)
+
+    # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
+    wine_path = os.path.join(os.path.dirname(os.path.relpath('__file__')), "wine-quality.csv")
+    data = pd.read_csv(wine_path)
+
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
+
+    # The predicted column is "quality" which is a scalar from [3, 9]
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+
+    alpha = float(args.alpha or 1.0)
+    l1_ratio = float(args.l1_ratio or 2.0)
+
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+
+        predicted_qualities = lr.predict(test_x)
+
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        print(f"Elasticnet model (alpha={alpha}, l1_ratio={l1_ratio}):")
+        print(f"  RMSE: {rmse}")
+        print(f"  MAE: {mae}")
+        print(f"  R2: {r2}")
+
+        dump(lr, os.path.join(output_dir, 'model'))
+
+        print(output_dir)
+        train_x.head().to_pickle(os.path.join(output_dir, 'head_input.pkl'))
+        train_y.head().to_pickle(os.path.join(output_dir, 'head_output.pkl'))
